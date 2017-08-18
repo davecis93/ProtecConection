fe148df8a3d20434891b1602365cb1b4142f30e4
davecis93 <david.cisneros@airsoftware.com.mx>
2017-08-18 16:51:13 -0500

    responseWS

--- src/main/java/com/airsoftware/protec/protec/control/ContactoTerminoController.java
+++ src/main/java/com/airsoftware/protec/protec/control/ContactoTerminoController.java
@@ -3,6 +3,7 @@ package com.airsoftware.protec.protec.control;
 
 import com.airsoftware.protec.protec.model.request.ConfirmaContactoDTO;
 import com.airsoftware.protec.protec.service.ContactoTerminoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,20 +13,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1")
 public class ContactoTerminoController {
 
-    ContactoTerminoService consultaPlacasContactoTermino;
+    @Autowired
+    private ContactoTerminoService consultaPlacasContactoTermino;
 
-    @GetMapping(value = "/consultaPlacas/{idOt}")
-    public ResponseEntity<ConfirmaContactoDTO> consultaPlacasContactoTermino(@PathVariable long idOt){
+    @GetMapping(value = "/consultaPlacas/{id}")
+    public ResponseEntity<Object> consultaPlacasContactoTermino(@PathVariable Long id){
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(consultaPlacasContactoTermino.consultaPlacasContactoTermino(idOt));
+                .body(consultaPlacasContactoTermino.consultaPlacasContactoTermino(id));
     }
 
-    @PostMapping(value = "/confirmarcontacto")
-    public ResponseEntity<ConfirmaContactoDTO> confirmaContactoTerminoServicio(@RequestBody ConfirmaContactoDTO confirmaContactoDTO){
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(consultaPlacasContactoTermino.confirmaContactoTerminoServicio(confirmaContactoDTO));
-    }
+//    @PostMapping(value = "/confirmarcontacto")
+//    public ResponseEntity<Object> confirmaContactoTerminoServicio(@RequestBody ConfirmaContactoDTO confirmaContactoDTO){
+//        return ResponseEntity
+//                .status(HttpStatus.OK)
+//                .body(consultaPlacasContactoTermino.confirmaContactoTerminoServicio(confirmaContactoDTO));
+//    }
 
 }
---
+++ src/main/java/com/airsoftware/protec/protec/service/impl/ContactoTrminosServiceImpl.java
@@ -0,0 +1,69 @@
+package com.airsoftware.protec.protec.service.impl;
+
+import com.airsoftware.protec.protec.service.ContactoTerminoService;
+import com.airsoftware.protec.protec.service.ProveedorService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+/**
+ * Created by areyna on 2/17/17.
+ */
+@Service
+@Transactional
+public class ContactoTrminosServiceImpl implements ContactoTerminoService {
+
+    private org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(ContactoTrminosServiceImpl.class);
+
+    @Value("http://201.161.41.142/wsRemoto2/")
+    public String PROTEC_ENDPOINT;
+
+    @Value("cmVtb3RvOnIzbTB0MCp2Mg==")
+    public String PROTEC_SECRET;
+
+
+    public Object consultaPlacasContactoTermino(Long idOt){
+        System.out.println(idOt);
+        Object response1 = new Object();
+
+        try{
+
+            logger.info(PROTEC_SECRET);
+
+            RestTemplate restTemplate = new RestTemplate();
+
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+            headers.set("Authorization", "Basic "+PROTEC_SECRET);
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+            MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+            map.add("idOt", ""+idOt+"");
+
+            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+
+            ResponseEntity<Object> response = restTemplate.postForEntity( PROTEC_ENDPOINT + "ConsultaPlacasContactoTermino/", request , Object.class );
+
+
+            System.out.println(response.getBody().toString());
+
+            response1 = response.getBody();
+
+        }catch(Exception ex) {
+            logger.error("Error al consultar el ws protec.", ex);
+        }
+
+        return response1;
+    }
+
+}
